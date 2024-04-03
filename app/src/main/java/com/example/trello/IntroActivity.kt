926e5d5dbe46b2c92f857d7f94326d63e3ee48e7
Darshan Kanani <darshankanani0007@gmail.com>
2024-04-03 16:23:02 +0530

    2 Setting Up The Intro Screen And Moving To It After 2 Second

--- app/src/main/java/com/example/trello/IntroActivity.kt
+++ app/src/main/java/com/example/trello/IntroActivity.kt
@@ -1,21 +1,16 @@
 package com.example.trello
 
-import android.annotation.SuppressLint
-import android.content.Intent
-import android.graphics.Typeface
 import android.os.Bundle
-import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.trello.databinding.ActivitySplashBinding
+import com.example.trello.databinding.ActivityIntroBinding
 
-@SuppressLint("CustomSplashScreen")
-class SplashActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySplashBinding
+class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivitySplashBinding.inflate(layoutInflater)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
@@ -25,12 +20,6 @@ class SplashActivity : AppCompatActivity() {
             insets
         }
 
-        val typeFace: Typeface = Typeface.createFromAsset(assets, "carbon bl.otf")
-        binding.tvAppName.typeface = typeFace
 
-        Handler().postDelayed({
-            startActivity(Intent(this,IntroActivity::class.java))
-            finish()
-        },2500)
     }
 }
