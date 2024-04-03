249788640cf342564218c869297f65909fb777bd
Darshan Kanani <darshankanani0007@gmail.com>
2024-04-03 14:41:58 +0530

    1 Setting Up The Splashscreen, Using Custom Fonts And How To Find Them

---
+++ app/src/main/java/com/example/trello/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.example.trello
+
+import android.annotation.SuppressLint
+import android.graphics.Typeface
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.trello.databinding.ActivitySplashBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val typeFace: Typeface = Typeface.createFromAsset(assets, "carbon bl.otf")
+        binding.tvAppName.typeface = typeFace
+
+    }
+}
