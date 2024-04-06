038e6baacbe2172aa49716a96b0e87b5ac29c2dd
Janvi Lathiya <jaanulathiya@gmail.com>
2024-04-06 20:16:24 +0530

    442

--- app/src/main/java/com/example/trello/activities/SignInActivity.kt
+++ app/src/main/java/com/example/trello/activities/SignInActivity.kt
@@ -1,41 +1,90 @@
 package com.example.trello.activities
 
+import android.content.Intent
+import android.nfc.Tag
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.trello.R
 import com.example.trello.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignInActivity : BaseActivity() {
     private lateinit var binding: ActivitySignInBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivitySignInBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
+
+        auth=FirebaseAuth.getInstance()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        binding.btnSignIn.setOnClickListener {
+           signInRegisteredUser()
+        }
+
         setupActionBar()
     }
 
     private fun setupActionBar() {
-
-        setSupportActionBar(binding.toolbarSignUpActivity)
+        setSupportActionBar(binding.toolbarSignInActivity)
 
         val actionBar = supportActionBar
-
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true)
             actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
         }
 
-        binding.toolbarSignUpActivity.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
-
+        binding.toolbarSignInActivity.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
     }
+
+    private fun signInRegisteredUser(){
+        val email: String = binding.etEmailSignIn.text.toString().trim { it <= ' ' }
+        val password: String = binding.etPasswordSignIn.text.toString().trim { it <= ' ' }
+
+        if (validateForm(email, password)){
+            showProgressDialog(resources.getString(R.string.please_wait))
+            auth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this) { task ->
+                    hideProgressDialog()
+                    if (task.isSuccessful){
+                        Log.d("sign in","signInWithEmail:success")
+                        val user=auth.currentUser
+                        startActivity(Intent(this, MainActivity::class.java))
+                    }else{
+                        Log.w("sign in","signInWithEmail:failure",task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+
+    private fun validateForm(email:String,password:String):Boolean{
+        return when{
+            TextUtils.isEmpty(email)-> {
+                showErrorSnackBar("Please enter an email address")
+                false
+            }
+            TextUtils.isEmpty(password)-> {
+                showErrorSnackBar("Please enter a password")
+                false
+            }else->{
+                true
+            }
+
+        }
+    }
+
 }
