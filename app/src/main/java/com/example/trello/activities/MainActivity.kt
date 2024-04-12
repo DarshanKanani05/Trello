0d6262b5a2ed9d3bb31b7a329ca87290ee674dc8
Darshan Kanani <darshankanani0007@gmail.com>
2024-04-12 13:58:11 +0530

    456

--- app/src/main/java/com/example/trello/activities/MainActivity.kt
+++ app/src/main/java/com/example/trello/activities/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.trello.activities
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
@@ -18,6 +20,10 @@ import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        const val MY_PROFILE_REQUEST_CODE = 11
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -72,11 +78,23 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
         })
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode==Activity.RESULT_OK&&requestCode== MY_PROFILE_REQUEST_CODE){
+            FirestoreClass().loadUserData(this)
+        }else{
+            Log.e("Cancelled","Cancelled")
+        }
+    }
+
 
     override fun onNavigationItemSelected(p0: MenuItem): Boolean {
         when (p0.itemId) {
             R.id.nav_my_profile -> {
-                startActivity(Intent(this,MyProfileActivity::class.java))
+                startActivityForResult(
+                    Intent(this, MyProfileActivity::class.java),
+                    MY_PROFILE_REQUEST_CODE
+                )
             }
 
             R.id.nav_sign_out -> {
