591204cfda4a1f62a1f88c13f8af2f727baa7685
Darshan Kanani <darshan.kanani.05@gmail.com>
2024-04-23 20:31:24 +0530

    474

--- app/src/main/java/com/example/trello/activities/MembersActivity.kt
+++ app/src/main/java/com/example/trello/activities/MembersActivity.kt
@@ -2,13 +2,17 @@ package com.example.trello.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.trello.R
 import com.example.trello.databinding.ActivityMembersBinding
+import com.example.trello.models.Board
+import com.example.trello.utils.Constants
 
 class MembersActivity : BaseActivity() {
     private lateinit var binding: ActivityMembersBinding
+    private lateinit var mBoardDetails: Board
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMembersBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
@@ -19,5 +23,24 @@ class MembersActivity : BaseActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        if (intent.hasExtra(Constants.BOARD_DETAIL)) {
+            mBoardDetails = intent.getParcelableExtra<Board>(Constants.BOARD_DETAIL)!!
+        }
+
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+
+        setSupportActionBar(binding.toolbarMembersActivity)
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+            actionBar.title = resources.getString(R.string.members)
+        }
+
+        binding.toolbarMembersActivity.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
     }
 }
