cdc50fc34aa6e456075f7cf70d7230f31e82bb55
Darshan Kanani <darshan.kanani.05@gmail.com>
2024-05-08 19:50:44 +0530

    483

---
+++ app/src/main/java/com/example/trello/dialogs/LabelColorListDialog.kt
@@ -0,0 +1,47 @@
+package com.example.trello.dialogs
+
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.trello.R
+import com.example.trello.adapters.LabelColorListItemsAdapter
+
+abstract class LabelColorListDialog(
+    context: Context,
+    private var list: ArrayList<String>,
+    private val title: String = "",
+    private var mSelectedColor: String = ""
+) : Dialog(context) {
+    private var adapter: LabelColorListItemsAdapter? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val view = LayoutInflater.from(context).inflate(R.layout.dialog_list, null)
+        setContentView(view)
+        setCanceledOnTouchOutside(true)
+        setCancelable(true)
+        setUpRecyclerView(view)
+    }
+
+    @SuppressLint("CutPasteId")
+    private fun setUpRecyclerView(view: View) {
+        view.findViewById<TextView>(R.id.tv_title).text = title
+        view.findViewById<RecyclerView>(R.id.rv_list).layoutManager = LinearLayoutManager(context)
+        adapter = LabelColorListItemsAdapter(context, list, mSelectedColor)
+        view.findViewById<RecyclerView>(R.id.rv_list).adapter = adapter
+
+        adapter!!.onItemClickListener = object : LabelColorListItemsAdapter.OnItemClickListener {
+            override fun onClick(position: Int, color: String) {
+                dismiss()
+                onItemSelected(color)
+            }
+        }
+    }
+
+    protected abstract fun onItemSelected(color: String)
+}
